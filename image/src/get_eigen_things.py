d418a0e875b3b8a06cb27ede6d41f67dac2a3f98
danyjabban <alexanderjabban@gmail.com>
2024-12-27 11:06:10 -0500

    input is sqs

--- image/src/get_eigen_things.py
+++ image/src/get_eigen_things.py
@@ -2,15 +2,19 @@ import json
 import numpy as np
 
 def lambda_handler(event, context):
-    matrix_list = json.loads(event)['matrix']
-    matrix = npa = np.asarray(matrix_list, dtype=np.float32)
-    eigen_values, eigen_vectors = np.linalg.eig(matrix)
-    eigen_values_list = eigen_values.tolist()
-    eigen_vectors_list = eigen_vectors.tolist()
+
+    for record in event['Records']:
+        message_body = record['body']
+        message_body = json.loads(message_body)
+        matrix_list = message_body['matrix']
+        matrix = np.asarray(matrix_list, dtype=np.float32)
+        eigen_values, eigen_vectors = np.linalg.eig(matrix)
+        eigen_values_list = eigen_values.tolist()
+        eigen_vectors_list = eigen_vectors.tolist()
 
     return {
         'statusCode': 200,
-        'body': json.dumps([eigen_values_list, eigen_vectors_list])
+        'body': f'successfully uploaded to db, {eigen_values_list}'
     }
 
 
@@ -21,6 +25,7 @@ def lambda_handler(event, context):
     
 #     d = {'message': 'Hello from Lambda! update made test CICD', 'matrix':mat.tolist()}
 #     j = json.dumps(d)
-#     out = lambda_handler(j, 'context')
-#     print(out)
+#     print(j)
+#     # out = lambda_handler(d, 'context')
+#     # print(out)
     
